b24d2971b155a05a2a7576c95ba6283f73c3dc9f
Josh Brown <JLBrown222@gmail.com>
2023-11-08 15:24:37 -0600

    Carousel mostly working

--- Cocktails/main.js
+++ Cocktails/main.js
@@ -5,16 +5,17 @@ function getDrink() {
     //capture the data input by the user
     let drink = document.getElementById("input").value
     //clear the existing array of drinks so that multiple user inputs doesn't create a massive drink list
-    document.querySelector("#root").innerHTML = ""
+    document.querySelector("#carousel").innerHTML = ""
 
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
     .then(res => res.json())
     .then(data => {
         console.log(data.drinks)
-
+        //create the DIVs that'll hold the content
         data.drinks.forEach( el => {
             //create a div for the drink that'll hold all the other elements.
             let containerDiv = document.createElement("div")
+                containerDiv.className = "carousel-item"
             //create an H2 for the name of the drink
             let drinkName = document.createElement("h2")
                 drinkName.innerHTML = el.strDrink
@@ -45,77 +46,57 @@ function getDrink() {
                 containerDiv.append(drinkImage)
                 containerDiv.append(drinkInstructions)
                 containerDiv.append(ingredientList)
-            //append the drink's container Div to the root div so it's rendered for the user
-                document.querySelector("#root").appendChild(containerDiv)
+            //append the drink's container Div to the root div, now called carousel, so it's rendered for the user
+                document.querySelector("#carousel").appendChild(containerDiv)
+
         })
+        //Creating the carousel
+        const carousel = document.querySelector(".carousel");
+        const prevButton = document.querySelector(".prev-button");
+        const nextButton = document.querySelector(".next-button");
+        let currentIndex = 0;
 
+        function updateCarousel() {
+            const items = document.querySelectorAll(".carousel-item");
+            items.forEach((item, index) => {
+                item.style.transform = `translateX(${100 * (index - currentIndex)}%)`;
+            })
+        }
 
+        function nextSlide() {
+            currentIndex = (currentIndex + 2) % carousel.childElementCount;
+            updateCarousel();
+        }
+    
+        function prevSlide() {
+            currentIndex = (currentIndex - 2 + carousel.childElementCount) % carousel.childElementCount;
+            updateCarousel();
+        }
 
-
-        //DRINK INGREDIENT LISTS
-        //create an array of ingredients from the 15 strings the API gives
-        // let ingredients = []
-        // for (let i = 1; i <= 15; i++) {
-        //     ingredients.push(
-        //         (data.drinks[0][`strMeasure`+i] || "") +
-        //         data.drinks[0][`strIngredient`+i])
-        // }
-        // ingredients = ingredients.filter(el => el !== "null")
-        // //clear any existing User result by emptying the element:
-        // document.querySelector(".ingredients").innerHTML = "" 
-        // //create a list item for each ingredient
-        // document.createElement("h3")
-        // ingredients.forEach(el => {
-        //     let unorderedList = document.createElement("ul")
-
-        //     let listItem = document.createElement("li")
-        //     listItem.innerHTML = el
-        //     unorderedList.append(listItem)
-
-        //     document.querySelector(".ingredients").appendChild(unorderedList)
-        // })
+        nextButton.addEventListener("click", nextSlide);
+        prevButton.addEventListener("click", prevSlide);
+        updateCarousel()
+        adjustCarouselItemHeight()
     })
     .catch(err => {
         console.log(`${err}`)
     });
 }
 
+function adjustCarouselItemHeight() {
+    const items = document.querySelectorAll(".carousel-item");
+    items.forEach(item => {
+        const children = item.children;
+        let totalHeight = 0;
 
-//API Ninjas' Drink API:
-// function getDrink() {
-//     let drink = document.getElementById("input").value
+        //loop through the children to calculate their heights
+        for (let i = 0; i < children.length; i++) {
+            totalHeight += children[i].clientHeight;
+        }
 
-//     fetch(`https://api.api-ninjas.com/v1/cocktail?name=${drink}`,
-//     {
-//         method: "GET",
-//         headers: {
-//             "X-Api-Key": "7ZGPMUYUEtwwbtojntbJIA==8TwVWhvfDKByqz3F"
-//         }
-//     })
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data)
-//         document.getElementById("image").src = data[0]
-//         document.getElementById("drinkName").innerText = data[0].name
-//         document.getElementById("instructions").innerText = data[0].instructions
-
-//         let ingredients = []
-//         for (let i = 0; i < data[0].ingredients.length; i++) {
-//             ingredients.push(data[0].ingredients[i])
-//         }
-
-//         document.createElement("h3")
-//         ingredients.forEach(el => {
-//             let unorderedList = document.createElement("ul")
-
-//             let listItem = document.createElement("li")
-//             listItem.innerHTML = el
-//             unorderedList.append(listItem)
-
-//             document.querySelector(".ingredients").appendChild(unorderedList)
-//         })
-//     })
-// }
+        item.style.minHeight = totalHeight + 'px'
+    })
+}
 
 //toDoList
-//display all results with a carousel of images
+// I have successfully made a (mostly) working carousel.  However now the CSS is fucked.  You can't see the entire DIV's content.  It's pushed upwards.
